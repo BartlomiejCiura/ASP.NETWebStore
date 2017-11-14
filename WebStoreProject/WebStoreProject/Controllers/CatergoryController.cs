94217a4ca8ebce481c1d807f94e1c5dbff334d7b
Molodiec <bartlomiej.ciura@gmail.com>
2017-11-14 20:30:29 +0100

    added database

---
+++ WebStoreProject/WebStoreProject/Controllers/CatergoryController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebStoreProject.Models;
+
+namespace WebStoreProject.Controllers
+{
+    public class CatergoryController : Controller
+    {
+        // GET: Catergory
+        public ActionResult Index()
+        {
+            using(DBModel db = new DBModel())
+            {
+                List<Category> categories = db.Category.ToList<Category>();
+                return View(categories);
+            }
+        }
+
+        // GET: Catergory/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Catergory/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Catergory/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Catergory/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Catergory/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Catergory/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Catergory/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
